6593c33fa86aa8cb964e916a9ae9015e1273e384
neha250 <75984798+neha250@users.noreply.github.com>
2020-12-21 16:01:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,56 +1,71 @@
 
+var wall, thickness;
+var bullet,speed,weight;
 
-var car,wall;
-
-
-var speed, weight;
-
-
-function setup(){
+function setup() {
   createCanvas(1600, 400);
 
+  speed=random(223,321);
+  weight=random(30,52);
 
-        speed=random(55,90);
-        weight=random(400,1500);
 
-
-        car=createSprite(50, 200, 50, 50);
-
-        car.velocityX = speed;
-
-        car.shapeColor=color(225);
+  thickness=random(22,83);
 
 
 
-        wall=createSprite(1500,200, 60, height/2)
-        wall.shapeColor=color(80,80,80)
+
+
+  bullet=createSprite(50, 200, 50,5);
+  bullet.velocityX=speed;
+  bullet.shapeColor=color(225);
+
+
+
+
+  wall=createSprite(1200, 200, thickness, height/2);
+
+  wall.shapeColor=color(230,230,230);
+//wall.shapeColor=color(80,80,80)
 }
 
 
 function draw() {
   background(0);
-
-  if(wall.x-car.x <(car.width+wall.width)/2)
+  //bullet.sprite.collide(wall.sprite,calulateDeformation)
+  if(hasCollide(bullet, wall))
 {
-         car.velocityX=0;
-         var deformation=0.5 = weight * speed * speed/22500;
-         if(deformation>180)
-        {
-                  car.shapeColor=color(225,0,0);
-        }
+  bullet.velocityX=0;
+  var damage=0.5 * weight * speed *speed/(thickness *thickness *thickness)
 
-        if(deformation>180 && deformation>100)
-        {
-                  car.shapeColor=color(230,230,0);  
-        }
 
-        if(deformation<100)
-        {
-                 car.shapeColor=color(0,225,0);
-        }
-      }
-      
-      drawSprites();
+  if(damage>10)
+{
+           wall.shapeColor=color(255,0,0);
 
-    }
-        
+}
+
+
+
+if(damage<10)
+{
+          wall.shapeColor=color(0,255,0);
+}
+
+}
+
+
+drawSprites();
+
+}
+
+
+function hasCollide(bullet, lwall)
+{
+          bulletRightEdge=lbullet.x +bullet.width;
+          wallLeftEdge=lwall.x;
+          if (bulletRightEdge>=wallLeftEdge)
+{
+          return true
+}
+          return false
+}          
